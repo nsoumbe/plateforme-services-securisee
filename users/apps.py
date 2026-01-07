251c0022327590f5d15b390f11692dfbb245373a
Josias SALOU <jsalou.ir2027@esaip.org>
2026-01-08 00:25:28 +0100

    Backend complet : Auth JWT, RBAC, Services et Commandesss

--- users/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users' # Assure-toi que ce n'est pas écrit 'services' ou autre chose ici
--- users/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-
-urlpatterns = [
-    # On laissera vide pour l'instant ou on ajoutera des routes plus tard
-]
